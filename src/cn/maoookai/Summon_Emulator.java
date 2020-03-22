7d52211eda385840a795b13325f81bd41f26501e
Maooookai <1220568032@qq.com>
2020-03-22 23:55:10 +0800

    reduce redundancy codes

--- src/cn/maoookai/Summon_Emulator.java
+++ src/cn/maoookai/Summon_Emulator.java
@@ -245,187 +245,39 @@ public class Summon_Emulator extends JcqAppAbstract implements ICQVer, IMsg, IRe
         return MSG_IGNORE;
     }
 
-    @Override
     public int discussMsg(int i, int i1, long l, long l1, String s, int i2) {
         return 0;
     }
 
-    /**
-     * 讨论组消息 (Type=4)<br>
-     * 本方法会在酷Q【线程】中被调用。<br>
-     *
-     * @param subtype     子类型，目前固定为1
-     * @param msgId       消息ID
-     * @param fromDiscuss 来源讨论组
-     * @param fromQQ      来源QQ号
-     * @param msg         消息内容
-     * @param font        字体
-     * @return 关于返回值说明, 见 {@link #privateMsg 私聊消息} 的方法
-     */
-    /*public int discussMsg(int subtype, int msgId, long fromDiscuss, long fromQQ, String msg, int font) {
-        // 这里处理消息
-
-        return MSG_IGNORE;
-    }
-
-     */
-
-    /**
-     * 群文件上传事件 (Type=11)<br>
-     * 本方法会在酷Q【线程】中被调用。<br>
-     *
-     * @param subType   子类型，目前固定为1
-     * @param sendTime  发送时间(时间戳)// 10位时间戳
-     * @param fromGroup 来源群号
-     * @param fromQQ    来源QQ号
-     * @param file      上传文件信息
-     * @return 关于返回值说明, 见 {@link #privateMsg 私聊消息} 的方法
-     */
     public int groupUpload(int subType, int sendTime, long fromGroup, long fromQQ, String file) {
         GroupFile groupFile = CQ.getGroupFile(file);
-        // 解析群文件信息，如果失败直接忽略该消息
-        // 这里处理消息
         return MSG_IGNORE;
     }
 
-    /**
-     * 群事件-管理员变动 (Type=101)<br>
-     * 本方法会在酷Q【线程】中被调用。<br>
-     *
-     * @param subtype        子类型，1/被取消管理员 2/被设置管理员
-     * @param sendTime       发送时间(时间戳)
-     * @param fromGroup      来源群号
-     * @param beingOperateQQ 被操作QQ
-     * @return 关于返回值说明, 见 {@link #privateMsg 私聊消息} 的方法
-     */
     public int groupAdmin(int subtype, int sendTime, long fromGroup, long beingOperateQQ) {
-        // 这里处理消息
-
         return MSG_IGNORE;
     }
 
-    /**
-     * 群事件-群成员减少 (Type=102)<br>
-     * 本方法会在酷Q【线程】中被调用。<br>
-     *
-     * @param subtype        子类型，1/群员离开 2/群员被踢
-     * @param sendTime       发送时间(时间戳)
-     * @param fromGroup      来源群号
-     * @param fromQQ         操作者QQ(仅子类型为2时存在)
-     * @param beingOperateQQ 被操作QQ
-     * @return 关于返回值说明, 见 {@link #privateMsg 私聊消息} 的方法
-     */
     public int groupMemberDecrease(int subtype, int sendTime, long fromGroup, long fromQQ, long beingOperateQQ) {
-        // 这里处理消息
-
         return MSG_IGNORE;
     }
 
-    /**
-     * 群事件-群成员增加 (Type=103)<br>
-     * 本方法会在酷Q【线程】中被调用。<br>
-     *
-     * @param subtype        子类型，1/管理员已同意 2/管理员邀请
-     * @param sendTime       发送时间(时间戳)
-     * @param fromGroup      来源群号
-     * @param fromQQ         操作者QQ(即管理员QQ)
-     * @param beingOperateQQ 被操作QQ(即加群的QQ)
-     * @return 关于返回值说明, 见 {@link #privateMsg 私聊消息} 的方法
-     */
     public int groupMemberIncrease(int subtype, int sendTime, long fromGroup, long fromQQ, long beingOperateQQ) {
-        // 这里处理消息
-
         return MSG_IGNORE;
     }
 
-    /**
-     * 好友事件-好友已添加 (Type=201)<br>
-     * 本方法会在酷Q【线程】中被调用。<br>
-     *
-     * @param subtype  子类型，目前固定为1
-     * @param sendTime 发送时间(时间戳)
-     * @param fromQQ   来源QQ
-     * @return 关于返回值说明, 见 {@link #privateMsg 私聊消息} 的方法
-     */
     public int friendAdd(int subtype, int sendTime, long fromQQ) {
-        // 这里处理消息
-
         return MSG_IGNORE;
     }
 
-    /**
-     * 请求-好友添加 (Type=301)<br>
-     * 本方法会在酷Q【线程】中被调用。<br>
-     *
-     * @param subtype      子类型，目前固定为1
-     * @param sendTime     发送时间(时间戳)
-     * @param fromQQ       来源QQ
-     * @param msg          附言
-     * @param responseFlag 反馈标识(处理请求用)
-     * @return 关于返回值说明, 见 {@link #privateMsg 私聊消息} 的方法
-     */
     public int requestAddFriend(int subtype, int sendTime, long fromQQ, String msg, String responseFlag) {
-        // 这里处理消息
-
-        /**
-         * REQUEST_ADOPT 通过
-         * REQUEST_REFUSE 拒绝
-         */
-
-        // CQ.setFriendAddRequest(responseFlag, REQUEST_ADOPT, null); // 同意好友添加请求
         return MSG_IGNORE;
     }
 
-    /**
-     * 请求-群添加 (Type=302)<br>
-     * 本方法会在酷Q【线程】中被调用。<br>
-     *
-     * @param subtype      子类型，1/他人申请入群 2/自己(即登录号)受邀入群
-     * @param sendTime     发送时间(时间戳)
-     * @param fromGroup    来源群号
-     * @param fromQQ       来源QQ
-     * @param msg          附言
-     * @param responseFlag 反馈标识(处理请求用)
-     * @return 关于返回值说明, 见 {@link #privateMsg 私聊消息} 的方法
-     */
     public int requestAddGroup(int subtype, int sendTime, long fromGroup, long fromQQ, String msg,
                                String responseFlag) {
-        // 这里处理消息
-
-        /**
-         * REQUEST_ADOPT 通过
-         * REQUEST_REFUSE 拒绝
-         * REQUEST_GROUP_ADD 群添加
-         * REQUEST_GROUP_INVITE 群邀请
-         */
-		/*if(subtype == 1){ // 本号为群管理，判断是否为他人申请入群
-			CQ.setGroupAddRequest(responseFlag, REQUEST_GROUP_ADD, REQUEST_ADOPT, null);// 同意入群
-		}
-		if(subtype == 2){
-			CQ.setGroupAddRequest(responseFlag, REQUEST_GROUP_INVITE, REQUEST_ADOPT, null);// 同意进受邀群
-		}*/
-
         return MSG_IGNORE;
     }
 
-    /**
-     * 本函数会在JCQ【线程】中被调用。
-     *
-     * @return 固定返回0
-     */
-    public int menuA() {
-        JOptionPane.showMessageDialog(null, "这是测试菜单A，可以在这里加载窗口");
-        return 0;
-    }
-
-    /**
-     * 本函数会在酷Q【线程】中被调用。
-     *
-     * @return 固定返回0
-     */
-    public int menuB() {
-        JOptionPane.showMessageDialog(null, "这是测试菜单B，可以在这里加载窗口");
-        return 0;
-    }
 
 }
